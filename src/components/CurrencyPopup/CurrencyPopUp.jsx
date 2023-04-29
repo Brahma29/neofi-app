7948efc7087816466bb38e0ec5e288f749dcd66f
Brahmanand Swami <124257410+Brahma21@users.noreply.github.com>
2023-04-29 18:11:20 +0530

    Created Crypto App

---
+++ src/components/CurrencyPopup/CurrencyPopUp.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import Icon from '../../assets/images/eth.png';
+import CrossIcon from '../../assets/images/cross.svg';
+import TickIcon from '../../assets/images/tick.svg';
+
+import './CurrencyPopUp.css';
+import axios from 'axios';
+
+const CurrencyPopUp = ({
+  isOpen,
+  setIsOpen,
+  selectedToken,
+  setSelectedToken,
+  tokens,
+}) => {
+  return (
+    <div className={`popup_wrapper ${isOpen && 'open'}`}>
+      <div className="popup_container">
+        <div className="popup_header">
+          <img
+            src={CrossIcon}
+            alt="cross_icon"
+            className="cross_icon"
+            onClick={() => setIsOpen(false)}
+          />
+        </div>
+        <div className="search_input">
+          <input type="text" placeholder="Search Chains" name="currency" />
+        </div>
+        <div className="listed_currencies">
+          {tokens.map((token) => (
+            <div
+              className={`listed_currency ${
+                selectedToken === token && 'selected'
+              }`}
+              onClick={() => {
+                setSelectedToken(token);
+                setIsOpen(false);
+              }}
+            >
+              <div className="currency_icon">
+                <img src={Icon} alt="icon" />
+                {token}
+              </div>
+              {selectedToken === token && (
+                <div className="tick">
+                  <img src={TickIcon} alt="tick" />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyPopUp;
---
+++ src/components/TradeCard/TradeCard.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { w3cwebsocket } from 'websocket';
+import './TradeCard.css';
+
+import Icon from '../../assets/images/eth.png';
+import DropIcon from '../../assets/images/drop-arrow.png';
+import CurrencyPopUp from '../CurrencyPopup/CurrencyPopUp';
+import axios from 'axios';
+
+const TradeCard = ({
+  tokens,
+  selectedToken,
+  currentPriceSelectedToken,
+  setSelectedToken,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [inputPrice, setInputPrice] = useState(null);
+  const [calculatedPrice, setCalculatedPrice] = useState(null);
+
+  const handleChange = (e) => {
+    setInputPrice(e.target.value);
+    setCalculatedPrice((prev) =>
+      (e.target.value / currentPriceSelectedToken).toFixed(5)
+    );
+  };
+
+  return (
+    <>
+      <div className="card">
+        <CurrencyPopUp
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          selectedToken={selectedToken}
+          setSelectedToken={setSelectedToken}
+          tokens={tokens}
+        />
+        <div className="icon_wrapper">
+          <div className="icon">
+            <img src={Icon} alt="eth" />
+          </div>
+        </div>
+        <div className="content">
+          <div className="current_value">
+            <span>Current value</span>
+            <span className="price">${currentPriceSelectedToken}</span>
+          </div>
+          <div className="currency_dropdown">
+            <button
+              className="currency_dropdown_button"
+              onClick={() => setIsOpen(true)}
+            >
+              <div className="selected_currency">
+                <img src={Icon} alt="selected_currency_icon" />
+                {selectedToken}
+              </div>
+              <div className="drop_icon">
+                <img src={DropIcon} alt="drop_icon" />
+              </div>
+            </button>
+          </div>
+          <div className="amount_input">
+            <label htmlFor="amount">Amount you want to invest</label>
+            <input
+              type="number"
+              placeholder="0.00"
+              id="amount"
+              value={inputPrice}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="currency_value">
+            <label htmlFor="calculated_amount">
+              Estimate Number of {selectedToken} You will Get
+            </label>
+            <input
+              type="number"
+              disabled
+              placeholder="0.00"
+              value={calculatedPrice}
+            />
+          </div>
+          <button className="buy_button">Buy</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TradeCard;
